4dc7a72ea80b7873cc02b3290e28e1232c5a4c8c
arepatiesa <carlosmarcos2004@gmail.com>
2023-11-15 14:06:23 -0400

    Revert "Exercise 1.14 anecdotes"
    This reverts commit 71ba954a9bc5508ba351277022813aaee2ab4fb1.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,71 +1,32 @@
-import React from "react";
-import { useState } from "react";
-
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
-
-const Anecdote = ({ anecdote, votes }) => {
-  return (
-    <div>
-      <p>{anecdote}</p>
-      <p>has {votes} votes</p>
-    </div>
-  );
-};
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const [points, setPoints] = useState({
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-  });
+  const [selected, setSelected] = useState(0)
+  const randomNum = () => {
+    setSelected(Math.floor(Math.random() * 7))
+  }
 
-  const [selected, setSelected] = useState(0);
-
-  const vote = () => {
-    setPoints({ ...points, [selected]: points[selected] + 1 });
-  };
-
-  const nextAnecdote = () => {
-    setSelected(Math.floor(Math.random() * 8));
-  };
-
-  let arr = Object.values(points);
-  let max = Math.max(...arr);
-  let index = arr.findIndex((x) => x == max);
-  let anecdote = anecdotes[selected];
-  let votes = points[selected];
-  let mostAnecdote = anecdotes[index];
-  let mostVotes = points[index];
+  console.log(selected)
 
   return (
     <div>
-      <h1>anecdote of the day</h1>
-      <Anecdote anecdote={anecdote} votes={votes} />
-      <Button handleClick={vote} text="vote" />
-      <Button handleClick={nextAnecdote} text="next anecdote" />
-
-      <h1>anecdote with most votes</h1>
-      <Anecdote anecdote={mostAnecdote} votes={mostVotes} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={randomNum}>next anecdote</button>
     </div>
-  );
-};
+  )
 
-export default App;
+}
+
+export default App
