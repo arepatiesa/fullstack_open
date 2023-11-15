c4c003d830e1de00fb5da69ab8d9a94f4008cbb2
arepatiesa <carlosmarcos2004@gmail.com>
2023-11-14 20:08:37 -0400

    Exercise 1.11 *I fucked up

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,22 +3,18 @@ import { useState } from "react";
 
 const StatisticLine = (props) => {
   return (
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
-  )
-}
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
 
 const Statistics = (props) => {
-  
   if (props.total == 0) {
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
+    return <p>No feedback given</p>;
   }
-  
+
   return (
     <div>
       <h1>statistics</h1>
@@ -28,22 +24,24 @@ const Statistics = (props) => {
           <StatisticLine text="neutral" value={props.neutral} />
           <StatisticLine text="bad" value={props.bad} />
           <StatisticLine text="total" value={props.total} />
-          <StatisticLine text="average" value={props.go} />
-          <StatisticLine text="good" value={props.score / props.total} />
-          <StatisticLine text="good" value={(props.good * 100) / props.total} />
+          <StatisticLine text="average" value={props.score / props.total} />
+          <StatisticLine
+            text="good"
+            value={(props.good * 100) / props.total + "%"}
+          />
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 const Button = (props) => {
   return (
     <div>
       <button onClick={props.handleClick}>{props.text}</button>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -54,18 +52,25 @@ const App = () => {
 
   const addGood = () =>
     setGood(good + 1, setTotal(total + 1), setAverage(score + 1));
-  const addNeutral = () => setNeutral(neutral + 1, setTotal(total + 1), setAverage(score + 0));
+  const addNeutral = () =>
+    setNeutral(neutral + 1, setTotal(total + 1), setAverage(score + 0));
   const addBad = () =>
     setBad(bad + 1, setTotal(total + 1), setAverage(score - 1));
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text={'good'} handleClick={addGood}/>
-      <Button text={'neutral'} handleClick={addNeutral}/>
-      <Button text={'bad'} handleClick={addBad}/>
+      <Button text={"good"} handleClick={addGood} />
+      <Button text={"neutral"} handleClick={addNeutral} />
+      <Button text={"bad"} handleClick={addBad} />
 
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} score={score}/>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        score={score}
+      />
     </div>
   );
 };
