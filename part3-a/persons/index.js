7b29a602a3431afe96d29f261488d74a4ee523eb
arepatiesa <carlosmarcos2004@gmail.com>
2024-01-22 07:02:53 -0400

    Exercise 3.8

--- part3-a/persons/index.js
+++ part3-a/persons/index.js
@@ -1,7 +1,10 @@
-const express = require('express') 
-const app = express()
-
-app.use(express.json())
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+app.use(express.json());
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
   {
@@ -28,85 +31,93 @@ let persons = [
 
 // GET persons
 app.get("/api/persons", (request, response) => {
-    response.send(persons)
-})
+  response.send(persons);
+});
 // GET info (num of people in array / current date)
 app.get("/", (request, response) => {
-    const people = persons.length
-    const date = new Date()
+  const people = persons.length;
+  const date = new Date();
 
-    response.send(`
+  response.send(`
         <p>Phonebook has info for ${people} people</p>
         <p>${date}</p>
-    `)
-})
+    `);
+});
 // GET single id
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-      response.json(person)
-    }
-    else {
-      response.status(400).send("That person doesn't exist.")
-    }
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(400).send("That person doesn't exist.");
+  }
+});
 // DELETE according to id
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const randomId = () => {
-  return Math.floor(Math.random() * 500)
-}
+  return Math.floor(Math.random() * 500);
+};
 // POST new person
-app.post("/api/persons", (request, response) => {
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body) {
     return response.status(400).json({
-      error: 'content missing'
-    })
+      error: "content missing",
+    });
   }
 
   if (!body.name) {
-    return response.status(400).json({
-      error: `name is missing`
-    }).end()
-  } 
-  else if (!body.number) {
-    return response.status(400).json({
-      error: `number is missing`
-    }).end()
-  } 
+    return response
+      .status(400)
+      .json({
+        error: `name is missing`,
+      })
+      .end();
+  } else if (!body.number) {
+    return response
+      .status(400)
+      .json({
+        error: `number is missing`,
+      })
+      .end();
+  }
 
-
-  const nameChecker = persons.some((obj) => obj.name.includes(body.name))
+  const nameChecker = persons.some((obj) => obj.name.includes(body.name));
 
   if (nameChecker == true) {
-    return response.status(409).json({
-      error: `name must be unique`
-    }).end()
+    return response
+      .status(409)
+      .json({
+        error: `name must be unique`,
+      })
+      .end();
   }
-  
+
   const person = {
     id: randomId(),
     name: body.name,
     number: body.number,
-  }
+  };
 
-  
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
