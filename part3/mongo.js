f11bd9909ad67870b25af93c0b4b30184f022a5d
arepatiesa <carlosmarcos2004@gmail.com>
2024-02-12 20:18:58 -0400

    Exercise 3.22

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const process = require('process');
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]  
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
-const url = `mongodb+srv://carlosmosorio:${password}@cluster0.limedqo.mongodb.net/phonebook?retryWrites=true&w=majority`
+const url = `mongodb+srv://carlosmosorio:${password}@cluster0.limedqo.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
     name: String,
     number: Number,
-})
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
 const person = Person({
     name: name,
     number: number,
-})
+});
 
-if (process.argv.length<4){
-    Person.find({}).then(persons => {
-        console.group('phonebook')
-        persons.forEach(person => {
+if (process.argv.length < 4) {
+    Person.find({}).then((persons) => {
+        console.group('phonebook');
+        persons.forEach((person) => {
             console.log(person.name, person.number);
-        })
-        mongoose.connection.close()
-    })
-}
-else {
-    person.save().then(result => {
+        });
+        mongoose.connection.close();
+    });
+} else {
+    person.save().then(() => {
         console.log(`Added ${name} number ${number} to phonebook`);
-        mongoose.connection.close()
-    })
+        mongoose.connection.close();
+    });
 }
